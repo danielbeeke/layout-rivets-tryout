7b9571633dd845e14d877fa44a32d2ebd609e2d7
Daniel Beeke <daniel@studiofonkel.nl>
2016-05-25 21:09:11 +0200

    Starting the layout with the correct state

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,89 +1,73 @@
-$( document ).ready(function() {
-
-    window.rivetsData = {
-        layout: {
-            sidebar: {
-                collapsed: false
-            },
-            panel: {
-                left: {
-                    name: 'left',
-                    expanded: true,
-                    collapsed: false
-                },
-                right: {
-                    name: 'right',
-                    expanded: false,
-                    collapsed: true
-                }
-            }
-        },
-
-        functions: {
-            toggleSidebar: function () {
-                rivetsData.layout.sidebar.collapsed = !rivetsData.layout.sidebar.collapsed;
-            },
-            setPanelLeftFullscreen: function () {
-                setPanelFullscreen('left');
-            },
-            setPanelRightFullscreen: function () {
-                setPanelFullscreen('right');
-            },
-            setPanelsBothMiddle: function () {
-                rivetsData.layout.panel.left = {
-                    expanded: false,
-                    collapsed: false
-                };
-
-                rivetsData.layout.panel.right = {
-                    expanded: false,
-                    collapsed: false
-                };
-            },
-            saveState: function () {
-                localStorage.setItem('layoutState', JSON.stringify(rivetsData.layout));
-            },
-            restoreState: function () {
-                var oldState = JSON.parse(localStorage.getItem('layoutState'));
-                rivetsData.layout = oldState;
-            }
-        }
-    };
-
-    function setPanelFullscreen(panelName) {
-        var oppositePanelName = panelName == 'left' ? 'right' : 'left';
-
-        rivetsData.layout.panel[panelName] = {
-            expanded: true,
-            collapsed: false
-        };
-
-        rivetsData.layout.panel[oppositePanelName] = {
-            expanded: false,
-            collapsed: true
-        };
-    }
-
-    rivets.bind($('#layout-container'), rivetsData);
-
-    // Paste the following in the console:
-
-    /*
-    rivetsData.layout = {
+window.rivetsData = {
+    layout: {
         sidebar: {
             collapsed: false
         },
         panel: {
             left: {
+                name: 'left',
                 expanded: true,
                 collapsed: false
             },
             right: {
+                name: 'right',
                 expanded: false,
                 collapsed: true
             }
         }
-    };
-    */
-});
+    },
 
+    functions: {
+        toggleSidebar: function () {
+            rivetsData.layout.sidebar.collapsed = !rivetsData.layout.sidebar.collapsed;
+        },
+        setPanelLeftFullscreen: function () {
+            setPanelFullscreen('left');
+        },
+        setPanelRightFullscreen: function () {
+            setPanelFullscreen('right');
+        },
+        setPanelsBothMiddle: function () {
+            rivetsData.layout.panel.left = {
+                expanded: false,
+                collapsed: false
+            };
+
+            rivetsData.layout.panel.right = {
+                expanded: false,
+                collapsed: false
+            };
+        },
+        saveState: function () {
+            localStorage.setItem('layoutState', JSON.stringify(rivetsData.layout));
+        },
+        restoreState: function () {
+            var oldState = JSON.parse(localStorage.getItem('layoutState'));
+            if (oldState) {
+                rivetsData.layout = oldState;
+            }
+        }
+    }
+};
+
+rivetsData.functions.restoreState();
+
+function setPanelFullscreen(panelName) {
+    var oppositePanelName = panelName == 'left' ? 'right' : 'left';
+
+    rivetsData.layout.panel[panelName] = {
+        expanded: true,
+        collapsed: false
+    };
+
+    rivetsData.layout.panel[oppositePanelName] = {
+        expanded: false,
+        collapsed: true
+    };
+}
+
+rivets.bind($('#layout-container'), rivetsData);
+
+setTimeout(function () {
+    $('body').addClass('transitions-enabled');
+}, 300);
