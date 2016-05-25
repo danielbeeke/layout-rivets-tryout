d2274aa77409c3866d79986ab5f260cb9291306a
Daniel Beeke <daniel@studiofonkel.nl>
2016-05-25 20:58:58 +0200

    Adding a couple of buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,61 @@
 $( document ).ready(function() {
+
     window.rivetsData = {
         layout: {
             sidebar: {
-                collapsed: true
+                collapsed: false
             },
             panel: {
                 left: {
-                    expanded: false,
-                    collapsed: true
-                },
-                right: {
+                    name: 'left',
                     expanded: true,
                     collapsed: false
+                },
+                right: {
+                    name: 'right',
+                    expanded: false,
+                    collapsed: true
                 }
+            }
+        },
+
+        functions: {
+            toggleSidebar: function () {
+                rivetsData.layout.sidebar.collapsed = !rivetsData.layout.sidebar.collapsed;
+            },
+            setPanelLeftFullscreen: function () {
+                setPanelFullscreen('left');
+            },
+            setPanelRightFullscreen: function () {
+                setPanelFullscreen('right');
+            },
+            setPanelsBothMiddle: function () {
+                rivetsData.layout.panel.left = {
+                    expanded: false,
+                    collapsed: false
+                };
+
+                rivetsData.layout.panel.right = {
+                    expanded: false,
+                    collapsed: false
+                };
             }
         }
     };
+
+    function setPanelFullscreen(panelName) {
+        var oppositePanelName = panelName == 'left' ? 'right' : 'left';
+
+        rivetsData.layout.panel[panelName] = {
+            expanded: true,
+            collapsed: false
+        };
+
+        rivetsData.layout.panel[oppositePanelName] = {
+            expanded: false,
+            collapsed: true
+        };
+    }
 
     rivets.bind($('#layout-container'), rivetsData);
 
