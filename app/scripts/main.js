14d2968b336b873252c1ffd51f2efe557a64bbad
Daniel Beeke <daniel@studiofonkel.nl>
2016-05-26 09:14:44 +0200

    code cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,53 @@
+window.octopus = {};
+
+octopus.layout = {
+    toggleSidebar: function () {
+        rivetsData.layout.sidebar.collapsed = !rivetsData.layout.sidebar.collapsed;
+    },
+    setPanelLeftBig: function () {
+        octopus.layout.setPanelBig('left');
+    },
+    setPanelRightBig: function () {
+        octopus.layout.setPanelBig('right');
+    },
+    setPanelsBothMiddle: function () {
+        rivetsData.layout.panel.left = {
+            expanded: false,
+            collapsed: false
+        };
+
+        rivetsData.layout.panel.right = {
+            expanded: false,
+            collapsed: false
+        };
+    },
+    saveState: function () {
+        localStorage.setItem('layoutState', JSON.stringify(rivetsData.layout));
+    },
+    restoreState: function () {
+        var oldState = JSON.parse(localStorage.getItem('layoutState'));
+        if (oldState) {
+            rivetsData.layout = oldState;
+        }
+    },
+    bothPanelsAreMedium: function (a, b) {
+        return a == b && a == false;
+    },
+    setPanelBig: function (panelName) {
+        var oppositePanelName = panelName == 'left' ? 'right' : 'left';
+
+        rivetsData.layout.panel[panelName] = {
+            expanded: true,
+            collapsed: false
+        };
+
+        rivetsData.layout.panel[oppositePanelName] = {
+            expanded: false,
+            collapsed: true
+        };
+    }
+};
+
 window.rivetsData = {
     layout: {
         sidebar: {
@@ -17,57 +67,11 @@ window.rivetsData = {
         }
     },
 
-    functions: {
-        toggleSidebar: function () {
-            rivetsData.layout.sidebar.collapsed = !rivetsData.layout.sidebar.collapsed;
-        },
-        setPanelLeftBig: function () {
-            setPanelBig('left');
-        },
-        setPanelRightBig: function () {
-            setPanelBig('right');
-        },
-        setPanelsBothMiddle: function () {
-            rivetsData.layout.panel.left = {
-                expanded: false,
-                collapsed: false
-            };
-
-            rivetsData.layout.panel.right = {
-                expanded: false,
-                collapsed: false
-            };
-        },
-        saveState: function () {
-            localStorage.setItem('layoutState', JSON.stringify(rivetsData.layout));
-        },
-        restoreState: function () {
-            var oldState = JSON.parse(localStorage.getItem('layoutState'));
-            if (oldState) {
-                rivetsData.layout = oldState;
-            }
-        },
-        bothPanelsAreMedium: function (a, b) {
-            return a == b && a == false;
-        }
-    }
+    // Reference the functions from our namespace so rivets can use them.
+    functions: octopus.layout
 };
 
-rivetsData.functions.restoreState();
-
-function setPanelBig(panelName) {
-    var oppositePanelName = panelName == 'left' ? 'right' : 'left';
-
-    rivetsData.layout.panel[panelName] = {
-        expanded: true,
-        collapsed: false
-    };
-
-    rivetsData.layout.panel[oppositePanelName] = {
-        expanded: false,
-        collapsed: true
-    };
-}
+octopus.layout.restoreState();
 
 rivets.bind($('#layout-container'), rivetsData);
 
